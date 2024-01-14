f7474c67d736fe853e0dc73a99e1ad5ed6ce167a
David Gann <davycrocket2003@gmail.com>
2024-01-13 23:33:35 -0700

    Wrote functions arrayAdd() and arrayMultiply()

--- functions.js
+++ functions.js
@@ -20,8 +20,28 @@ function arrayAdd(num1, num2) {
 }
 
 function arrayMultiply(num1, num2) {
-    
+    let digitProduct = [0]
+    let carry = 0
+    let result = [0]
+    let smallProduct
+    for (let i = 0; i < num1.length; i ++) {
+        digitProduct = Array(i).fill(0)
+        carry = 0
+        for (let j = 0; j < num2.length; j ++) {
+            smallProduct = num1[i] * num2[j] + carry
+            digitProduct.push(smallProduct % 10)
+            carry = Math.floor(smallProduct / 10)
+        }
+        digitProduct.push(carry)
+        result = arrayAdd(result, digitProduct)
+    }
+    return result
+
 }
 
 
-console.log(arrayAdd([1, 0, 0], [0, 9, 0]))
+// console.log(arrayMultiply([1, 2, 9, 5, 1], [7, 6, 5, 0, 9, 0]))
+module.exports = {
+    arrayAdd,
+    arrayMultiply
+}
