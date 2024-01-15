cdab75adeff3d9582c77e6ab954a8616d6438e23
David Gann <davycrocket2003@gmail.com>
2024-01-14 22:15:01 -0700

    57 is solved! :)

--- 57.js
+++ 57.js
@@ -7,7 +7,8 @@ let bottom = [1]
 let count = 0
 let tempN
 let max = 1000
-let remainder
+let r1
+let r2
 
 function countDigits(n) {
     let c = 0
@@ -21,15 +22,20 @@ function countDigits(n) {
 
 
 for (let i = 1; i <= max; i ++) {
-    remainder = myFunctions.arrayAdd(bottom, top)
-    if (remainder.length > bottom.length) {
+
+    r1 = myFunctions.arrayAdd(bottom, top)
+    r2 = top
+    if (r1.length > top.length) {
         count ++
+        
     }
     tempN = myFunctions.arrayAdd(bottom, myFunctions.arrayMultiply([2], top))
     bottom = top
     top = tempN
+
 }
 
 console.log(count)
-console.log(top)
-console.log(bottom)
+console.log(r1)
+console.log(r2)
+// console.log(bottom)
