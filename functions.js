d14dc3d80cb6bec3b7cdac4bd0ffb7fe978836c1
David Gann <davycrocket2003@gmail.com>
2024-01-16 10:44:13 -0700

    Fixed an error in bigPrimes()

--- functions.js
+++ functions.js
@@ -45,25 +45,78 @@ function arrayMultiply(num1, num2) {
 
 //Function that generates a list of primes up to a given number (not optimized aka slow)
 function listPrimes(num) {
-    let candidates = Array(Math.floor(num / 2)).fill(true)
+    if (num % 2 === 0) {
+        num --
+    }
+    let candidates = Array((num - 1) / 2).fill(true)
     let result = [2]
     let mode = 3
     while (mode < num ** 0.5 + 1) {
         for (let j = mode * mode; j <= num; j += 2 * mode) {
-            candidates[j/2 -1.5] = false
+            candidates[(j - 3) / 2] = false
         }
         mode += 2
-        while (candidates[mode / 2 - 1.5] === 0) {
+        while (candidates[(mode - 3) / 2] === false) {
             mode += 2
         }
     }
     for (let i = 0; i < candidates.length; i ++) {
         if (candidates[i]) {
-            result.push((i + 1.5) * 2)
+            result.push(i * 2 + 3)
         }
     }
     return result
     
+}
+
+
+//Internal function to support bigPrimes
+function primeSearch(low, high, givenPrimes) {
+    if (low % 2 === 0) {
+        low ++
+    }
+    if (high % 2 === 0) {
+        high --
+    }
+    let candidates = Array((high - low) / 2 + 1).fill(true)
+    let newPrimes = []
+
+    //A eliminate odd multiples of given primes
+    for (let i = 1; i < givenPrimes.length; i ++) {
+        let p = givenPrimes[i]              //Grab prime at index i (skips 2 (i=0))
+        let r = low % p
+        let p1 = low
+        if (r !== 0) {
+            p1 = p + low - r    //Set p1 to the first multiple of p within given range
+        }
+            if (p1 % 2 === 0) {
+            p1 += p                         //If p1 is even, choose the next multiple of p
+        }
+        for (let j = p1; j <= high; j += 2 * p) {
+            candidates[(j - low) / 2] = false
+        }
+    }
+
+    //B walk through range collecting primes and eliminating multiples (if needed)
+    let index = 0
+    while (candidates[index] === false) {
+        index ++
+    }
+    while (index < candidates.length) {
+        let p = index * 2 + low
+        newPrimes.push(p)
+        //Cross out multiples of current prime
+        for (let i = 3 * p; i <= high; i += 2 * p) {
+            candidates[(i - low) / 2] = false
+        }
+        index ++
+        while (candidates[index] === false) {
+            index ++
+        }
+    }
+
+    //C Return list of new primes
+    return newPrimes
 }
 
 //Function to compute primes above 100 million
@@ -74,51 +127,7 @@ function bigPrimes(n) {
         return listPrimes(n) 
     }
     primeList = listPrimes(hundredMillion)
-    //internal function to search a given range for primes (requires prime list below given range)
-    function primeSearch(low, high, givenPrimes) {
-        if (low % 2 === 0) {
-            low ++
-        }
-        if (high % 2 === 0) {
-            high --
-        }
-        let candidates = Array((high - low) / 2 + 1).fill(true)
-        let newPrimes = []
-
-        //A eliminate odd multiples of given primes
-        for (let i = 1; i < givenPrimes.length; i ++) {
-            let p = givenPrimes[i]          //Grab prime at index i (skips 2 (i=0))
-            let p1 = p + low - (low % p)    //Set p1 to the first multiple of p within given range
-            if (p1 % 2 === 0) {
-                p1 += p                     //If p1 is even, choose the next multiple of p
-            }
-            for (let j = p1; j <= high; j += 2 * p) {
-                candidates[(j - low) / 2] = false
-            }
-        }
-
-        //B walk through range collecting primes and eliminating multiples (if needed)
-        let index = 0
-        index ++
-        while (candidates[index] === false) {
-            index ++
-        }
-        while (index < candidates.length) {
-            let p = index * 2 + low
-            newPrimes.push(p)
-            //Cross out multiples of current prime
-            for (let i = 3 * p; i <= high; i += 2 * p) {
-                candidates[(i - low) / 2] = false
-            }
-            index ++
-            while (candidates[index] === false) {
-                index ++
-            }
-        }
-
-        //C Return list of new primes
-        return newPrimes
-    }
+    
 
     let low = hundredMillion
     let high
@@ -136,10 +145,22 @@ function bigPrimes(n) {
 
 
 
-// console.log(bigPrimes(100000000))
+// process.stdout.write(JSON.stringify(bigPrimes(1000000000)) + '\n')
 module.exports = {
     arrayAdd,
     arrayMultiply,
     listPrimes,
     bigPrimes
 }
+
+
+// debug stuff
+// let list = listPrimes(100)
+// console.log(primeSearch(100, 200, list))
+
+
+// for (let i = 100; i < 1000; i += 100) {
+//     list = list.concat(primeSearch(i, i + 100, list))
+// }
+// process.stdout.write(JSON.stringify(list) + '\n')
+// console.log(list.length)
