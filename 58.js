0204fad0592b1d34eabdedb14369e06a933269b3
David Gann <davycrocket2003@gmail.com>
2024-01-16 01:52:34 -0700

    58 seems to work but answer is wrong :(

--- 58.js
+++ 58.js
@@ -2,13 +2,13 @@
 
 const myFunctions = require('./functions')          //Access my functions
 
-let maxWidth = 10001                                 //Stop searching after this width of square
-let primeSearch = 100                               //Prime search threshold. When exceeded, multiply by 10
-let primes = myFunctions.listPrimes(primeSearch)    //Working list of primes
+let maxWidth = 7                                //Stop searching after this width of square
+let primeSearch = 100000000                         //Prime search threshold. When exceeded, multiply by 2
+let primes = myFunctions.bigPrimes(primeSearch)     //Working list of primes
 function isPrime(num) {                             //Function to determine if a number is prime
     while (num > primeSearch) {
-        primeSearch *= 10
-        primes = myFunctions.listPrimes(primeSearch)
+        primeSearch *= 2
+        primes = myFunctions.bigPrimes(primeSearch)
     }
     return primes.includes(num)
 }
@@ -25,6 +25,7 @@ while (primeCount / diagonalCount >= 0.1 && edgeLength < maxWidth) {
     }
     diagonalCount += 4
     edgeLength += 2
+    console.log(edgeLength, primeCount/diagonalCount)
 }
 
 console.log(edgeLength)
